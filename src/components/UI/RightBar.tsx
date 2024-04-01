4896d55a94cc86edd554ded3580cc8eb1751f7c0
b21827483 <b21827483@cs.hacettepe.edu.tr>
2024-04-01 18:27:39 +0300

    right bar added

--- src/components/UI/RightBar.tsx
+++ src/components/UI/RightBar.tsx
@@ -1,7 +1,69 @@
 import '../../../styles/RightBar.scss'
 
 function RightBar() {
-    return <div className='rightbar'>Right Bar</div>
+    return (
+        <div className='rightbar'>
+            <div className='container'>
+                <div className='recent-activities'>
+                    <h2>Latest Activities</h2>
+                    <div>
+                        <div>Activity 1</div>
+                        <div>Activity 1</div>
+                        <div>Activity 1</div>
+                        <div>Activity 1</div>
+                    </div>
+                </div>
+                <div className='online-friends-container'>
+                    <div className='header'>
+                        <h2>Online Friends</h2>
+                    </div>
+                    <div className='online-friends'>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                           <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                        <div>friend 1</div>
+                    </div>
+                </div>
+
+                <div className='extra'>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                    <div>friend 1</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RightBar
